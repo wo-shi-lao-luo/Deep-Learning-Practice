91c787677159e531bac530297810985d3ec85316
kyle-luo <tsjlky@gmail.com>
2020-02-12 16:56:31 -0500

    UPDATE

---
+++ Week 4/C4-Application ANN - Churn Modeling.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 30 11:34:42 2019
+
+@author: wchen
+"""
+# In[1]:
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+# Importing the dataset
+# os.chdir("C:/Users/wei_chen/Desktop/##2020Spring/week4")
+dataset = pd.read_csv('C4-Churn_Modelling.csv')
+dataset2 = dataset.iloc[:,3:14]
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# In[2]:
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+# Convert 'Male' to 1, 'Female' to 0
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+
+# Country column
+ct = ColumnTransformer([("Country", OneHotEncoder(), [1])], remainder = 'passthrough')
+X = ct.fit_transform(X)
+
+# In[3]:
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# In[4]:
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+# In[5]:
+# Part 2 - Now let's make the ANN!
+
+# Importing the Keras libraries and packages
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 12))
+
+# Adding the second hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+
+# Adding the output layer
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 20)
+
+# In[6]:
+
+# Part 3 - Making predictions and evaluating the model
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5).flatten()*1
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+print(cm)
+print('Accuracy:', (cm[0,0]+cm[1,1])/np.sum(cm) ) 
+
+import numpy as np
+unique, counts = np.unique(y_test, return_counts=True)
+print(dict(zip(unique, counts)))
+
+# In[7]:
+# Predicting a single new observation
+"""Predict if the customer with the following informations will leave the bank:
+Geography: France
+Credit Score: 600
+Gender: Male
+Age: 40
+Tenure: 3
+Balance: 60000
+Number of Products: 2
+Has Credit Card: Yes
+Is Active Member: Yes
+Estimated Salary: 50000"""
+new_probability = classifier.predict(sc.transform(np.array([[1, 0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]])))
+new_prediction = (new_probability > 0.5)
+
+# In[8]:
+# Calculate Root Mean Square Error
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+rms = sqrt(mean_squared_error(y_test, y_pred.flatten()*1))
+print(rms)
+
+rms = sqrt(sum((y_test-y_pred)**2)/len(y_test))
+print(rms)
